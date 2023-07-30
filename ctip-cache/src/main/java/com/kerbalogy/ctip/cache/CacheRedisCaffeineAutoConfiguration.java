7842070239dd6827558574db4b05a6eb175f12e1
Artistzq <yaozongqing@outlook.com>
2023-07-31 01:05:45 +0800

    feat: 实现二级缓存

--- ctip-cache/src/main/java/com/kerbalogy/ctip/cache/CacheRedisCaffeineAutoConfiguration.java
+++ ctip-cache/src/main/java/com/kerbalogy/ctip/cache/CacheRedisCaffeineAutoConfiguration.java
@@ -1,11 +1,20 @@
 package com.kerbalogy.ctip.cache;
 
+import com.kerbalogy.ctip.cache.base.CacheMessageListener;
+import com.kerbalogy.ctip.cache.base.RedisCaffeineCacheManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import java.util.Objects;
 
 /**
  * @author yaozongqing@outlook.com
@@ -21,5 +30,28 @@ public class CacheRedisCaffeineAutoConfiguration {
     @Autowired
     private CacheRedisCaffeineProperties cacheRedisCaffeineProperties;
 
+    @Bean("L2CacheManager")
+    @ConditionalOnBean(RedisTemplate.class)
+    public RedisCaffeineCacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+        log.info("===========================================");
+        log.info("=                                         =");
+        log.info("=          Two Level Cache Start          =");
+        log.info("=                                         =");
+        log.info("===========================================");
+        return new RedisCaffeineCacheManager(cacheRedisCaffeineProperties, redisTemplate);
+    }
+
+    @Bean
+    @ConditionalOnBean(RedisTemplate.class)
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisTemplate<Object, Object> redisTemplate, RedisCaffeineCacheManager redisCaffeineCacheManager) {
+        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
+        redisMessageListenerContainer.setConnectionFactory(Objects.requireNonNull(redisTemplate.getConnectionFactory()));
+        CacheMessageListener cacheMessageListener = new CacheMessageListener(redisTemplate, redisCaffeineCacheManager);
+        redisMessageListenerContainer.addMessageListener(cacheMessageListener, new ChannelTopic(
+                cacheRedisCaffeineProperties.getRedis().getTopic()
+        ));
+        return redisMessageListenerContainer;
+    }
 
 }
--- ctip-cache/src/main/java/com/kerbalogy/ctip/cache/base/RedisCaffeineCacheManager.java
+++ ctip-cache/src/main/java/com/kerbalogy/ctip/cache/base/RedisCaffeineCacheManager.java
@@ -1,16 +1,18 @@
 package com.kerbalogy.ctip.cache.base;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
 import com.kerbalogy.ctip.cache.CacheRedisCaffeineProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yaozongqing@outlook.com
@@ -45,6 +47,62 @@ public class RedisCaffeineCacheManager implements CacheManager {
         this.dynamic = this.cacheRedisCaffeineProperties.isDynamic();
     }
 
+    public void clearAllCache() {
+        redisTemplate.convertAndSend(cacheRedisCaffeineProperties.getRedis().getTopic(), new CacheMessage(null, null));
+    }
+
+    public static Map<String, CacheStats> getCacheStats() {
+        if (CollectionUtils.isEmpty(cacheMap)) {
+            return null;
+        }
+
+        Map<String, CacheStats> result = new LinkedHashMap<>();
+        for (Cache cache: cacheMap.values()) {
+            RedisCaffeineCache redisCaffeineCache = (RedisCaffeineCache) cache;
+            result.put(redisCaffeineCache.getName(), redisCaffeineCache.getCaffeineCache().stats());
+        }
+        return result;
+    }
+
+    private com.github.benmanes.caffeine.cache.Cache<Object, Object> caffeineCache(String name) {
+        Caffeine<Object, Object> cacheBuilder = Caffeine.newBuilder();
+        CacheRedisCaffeineProperties.CacheDefault cacheConfig = switch (name) {
+            case CacheNames.CACHE_15MIN -> cacheRedisCaffeineProperties.getCache15m();
+            case CacheNames.CACHE_30MIN -> cacheRedisCaffeineProperties.getCache30m();
+            case CacheNames.CACHE_60MIN -> cacheRedisCaffeineProperties.getCache60m();
+            case CacheNames.CACHE_180MIN -> cacheRedisCaffeineProperties.getCache180m();
+            default -> cacheRedisCaffeineProperties.getCacheDefault();
+        };
+        long expireAfterAccess = cacheConfig.getExpireAfterAccess();
+        long expireAfterWrite = cacheConfig.getExpireAfterWrite();
+        int initialCapacity = cacheConfig.getInitialCapacity();
+        long maximumSize = cacheConfig.getMaximumSize();
+        long refreshAfterWrite = cacheConfig.getRefreshAfterWrite();
+
+        log.debug("本地缓存初始化");
+        if (expireAfterAccess > 0) {
+            log.debug("设置本地缓存访问后过期时间，{}秒", expireAfterAccess);
+            cacheBuilder.expireAfterAccess(expireAfterAccess, TimeUnit.SECONDS);
+        }
+        if (expireAfterWrite > 0) {
+            log.debug("设置本地缓存写入后过期时间，{}秒", expireAfterWrite);
+            cacheBuilder.expireAfterWrite(expireAfterWrite, TimeUnit.SECONDS);
+        }
+        if (initialCapacity > 0) {
+            log.debug("设置缓存初始化大小{}", initialCapacity);
+            cacheBuilder.initialCapacity(initialCapacity);
+        }
+        if (maximumSize > 0) {
+            log.debug("设置本地缓存最大值{}", maximumSize);
+            cacheBuilder.maximumSize(maximumSize);
+        }
+        if (refreshAfterWrite > 0) {
+            cacheBuilder.refreshAfterWrite(refreshAfterWrite, TimeUnit.SECONDS);
+        }
+        cacheBuilder.recordStats();
+        return cacheBuilder.build();
+    }
+
     public RedisCaffeineCacheManager() {
     }
 
@@ -66,11 +124,22 @@ public class RedisCaffeineCacheManager implements CacheManager {
 
     @Override
     public Cache getCache(String name) {
-        return null;
+        Cache cache = cacheMap.get(name);
+        if (cache != null) {
+            return cache;
+        }
+        if (! dynamic && ! cacheNames.contains(name)) {
+            return null;
+        }
+
+        cache = new RedisCaffeineCache(name, redisTemplate, caffeineCache(name), cacheRedisCaffeineProperties);
+        Cache oldCache = cacheMap.putIfAbsent(name, cache);
+        log.debug("创建缓存实例：名称=[{}]", name);
+        return oldCache == null ? cache : oldCache;
     }
 
     @Override
     public Collection<String> getCacheNames() {
-        return null;
+        return this.cacheNames;
     }
 }
